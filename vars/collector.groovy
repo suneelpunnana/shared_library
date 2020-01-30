9b1f3ea0d9c42a56b9705c5c245aa1ee66158953
suneelpunnana <55619931+suneelpunnana@users.noreply.github.com>
2020-01-30 22:56:12 +0530

    Update collector.groovy

--- vars/collector.groovy
+++ vars/collector.groovy
@@ -1,5 +1,5 @@
 def call(){
 
-sh 'curl -X GET http://3.15.229.74:8080//api/json?pretty=true --user admin:admin'
+sh 'curl -X GET http://3.15.229.74:8080//api/json?pretty=true --user admin:11cfd35185f955e0f4f547669ee81ebc7a'
 
 }
