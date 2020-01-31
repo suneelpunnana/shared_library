1997ff7ee0f1f7a46c2dde70b337837edc29c2d9
suneelpunnana <55619931+suneelpunnana@users.noreply.github.com>
2020-01-31 11:47:59 +0530

    Update Build.groovy

--- vars/Build.groovy
+++ vars/Build.groovy
@@ -1,4 +1,4 @@
-def call()
+def call(jobname)
 {
-sh 'curl -s http://3.15.229.74:8080/job/grovy/lastBuild/api/json -u admin:11cfd35185f955e0f4f547669ee81ebc7a'
+  sh "curl -s http://3.15.229.74:8080/job/${jobname}/lastBuild/api/json -u admin:11cfd35185f955e0f4f547669ee81ebc7a"
 }
