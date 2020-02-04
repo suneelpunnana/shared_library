22bf43951b074f1de9c022fd400d41e7b8d81516
suneelpunnana <55619931+suneelpunnana@users.noreply.github.com>
2020-02-04 19:19:44 +0530

    Delete buildstatus.groovy

--- vars/buildstatus.groovy
+++
@@ -1,4 +0,0 @@
- def call(jobname,BUILD_NUMBER)
- {
-  sh curl -s http://3.15.229.74/:8080/job/'${jobname}'/'$(BUILD_NUMBER)'/buildstatus/api/json --user admin:11cfd35185f955e0f4f547669ee81ebc7a"
-   }
