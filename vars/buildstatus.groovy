bd6245e8d0b60241a219e0d97f34582d8e718ea0
suneelpunnana <55619931+suneelpunnana@users.noreply.github.com>
2020-02-04 18:49:36 +0530

    Update buildstatus.groovy

--- vars/buildstatus.groovy
+++ vars/buildstatus.groovy
@@ -1,5 +1,4 @@
  def call(jobname,BUILD_NUMBER)
  {
-  sh "curl --silent http://admin:11cfd35185f955e0f4f547669ee81ebc7a@http://3.15.229.74:8080/job/${JOB_NAME}/${BUILD_NUMBER}/api/json"
-
- }
+  sh curl -s http://3.15.229.74/:8080/job/'${jobname}'/'$(BUILD_NUMBER)'/buildstatus/api/json --user admin:11cfd35185f955e0f4f547669ee81ebc7a"
+   }
