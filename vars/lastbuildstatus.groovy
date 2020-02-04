00753295afbfde73efdc9887ff2b828542280f87
suneelpunnana <55619931+suneelpunnana@users.noreply.github.com>
2020-02-04 18:06:55 +0530

    Update lastbuildstatus.groovy

--- vars/lastbuildstatus.groovy
+++ vars/lastbuildstatus.groovy
@@ -1,3 +1,3 @@
- def call(jobname)
+ def call(jobname,BUILD_NUMBER)
  {
- curl --silent http://admin:TokenID@Jenkins-BuildURL/job/${jobname}/${BUILD_NUMBER}/api/json
+ sh "curl --silent http://admin:11cfd35185f955e0f4f547669ee81ebc7a@http://3.15.229.74:8080/job/${jobname}/${BUILD_NUMBER}/api/json"
