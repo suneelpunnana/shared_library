52418904d944ca9a0abbdec29dcf4fae54d52ab3
suneelpunnana <55619931+suneelpunnana@users.noreply.github.com>
2020-02-04 16:04:30 +0530

    Update createjob.groovy

--- vars/createjob.groovy
+++ vars/createjob.groovy
@@ -7,13 +7,13 @@
 
 def call(dsl,jobname){
 
-static url='http://18.221.47.136:8080/createItem?name='+jobname
+static url='http://3.15.229.74:8080/createItem?name='+jobname
 
 sh 'curl -O https://repo.jenkins-ci.org/public/org/jenkins-ci/plugins/job-dsl-core/1.76/job-dsl-core-1.76-standalone.jar'
 
 sh "java -jar job-dsl-core-1.76-standalone.jar '${dsl}'"
 
-url='http://18.221.47.136:8080/createItem?name='+jobname
+url='http://3.15.229.74:8080/createItem?name='+jobname
 sh "echo ${url}"
-sh "curl -s -XPOST  '$url' -u admin:119767fb81f22e2f10d8594e4201717e53 --data-binary @Test.xml -H Content-Type:text/xml"
+sh "curl -s -XPOST  '$url' -u admin:11cfd35185f955e0f4f547669ee81ebc7a --data-binary @Test.xml -H Content-Type:text/xml"
 }
