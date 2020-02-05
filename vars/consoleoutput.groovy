0e0621ac864ee992198c70347482af0f26d6ab0a
suneelpunnana <55619931+suneelpunnana@users.noreply.github.com>
2020-02-05 16:10:59 +0530

    Update consoleoutput.groovy

--- vars/consoleoutput.groovy
+++ vars/consoleoutput.groovy
@@ -1,6 +1,6 @@
 def call()
 {
-  sh "curl -X GET http://3.15.229.74:8080/job/jersey/lastBuild/consoleText -u admin:11cfd35185f955e0f4f547669ee81ebc7a -O hi.txt "
+  sh "curl -X GET http://3.15.229.74:8080/job/jersey/lastBuild/consoleText -u admin:11cfd35185f955e0f4f547669ee81ebc7a -O console.txt "
 //sh "wget --user=admin --password='11cfd35185f955e0f4f547669ee81ebc7a' http://3.15.229.74:8080/job/jersey/lastBuild/consoleText"  
 //sh "curl -s  http://3.15.229.74:8080/job/jersey/lastBuild/consoleText -u admin:11cfd35185f955e0f4f547669ee81ebc7a -o console.txt"
 }
