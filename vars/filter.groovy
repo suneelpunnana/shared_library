cbf10be70c2730a82a94908180b22817b2336c0b
suneelpunnana <55619931+suneelpunnana@users.noreply.github.com>
2020-02-05 17:18:14 +0530

    Update filter.groovy

--- vars/filter.groovy
+++ vars/filter.groovy
@@ -1,3 +1,3 @@
 def call(){
-sh curl --globoff -user admin:11cfd35185f955e0f4f547669ee81ebc7a http://3.15.229.74:8080/view/All /api/json?tree=jobs[name,builds[number,actions[parameters[name,value]]]]&pretty=true
+sh "curl --globoff -user admin:11cfd35185f955e0f4f547669ee81ebc7a http://3.15.229.74:8080/view/All /api/json?tree=jobs[name,builds[number,actions[parameters[name,value]]]]&pretty=true"
 }
