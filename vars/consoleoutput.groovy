84a0d77bca9f5f0170b15c89848516ebdf3b6928
suneelpunnana <55619931+suneelpunnana@users.noreply.github.com>
2020-02-05 15:53:42 +0530

    Update consoleoutput.groovy

--- vars/consoleoutput.groovy
+++ vars/consoleoutput.groovy
@@ -1,4 +1,4 @@
 def call()
 {
-sh "curl -s  http://3.15.229.74:8080/job/jersey/lastBuild/consoleText -u admin:11cfd35185f955e0f4f547669ee81ebc7a -Outfile console.txt"
+sh "curl -s  http://3.15.229.74:8080/job/jersey/lastBuild/consoleText -u admin:11cfd35185f955e0f4f547669ee81ebc7a >> console.txt"
 }
