9fc1f689a3c26326022a5c790ed0e154c90acb5b
suneelpunnana <55619931+suneelpunnana@users.noreply.github.com>
2020-02-05 17:49:14 +0530

    Update pluginlist.groovy

--- vars/pluginlist.groovy
+++ vars/pluginlist.groovy
@@ -1,4 +1,4 @@
 def call(){
 sh "curl -o http://3.15.229.74:8080/jnlpJars/jenkins-cli.jar"
-sh "java -jar jenkins-cli.jar -s http://3.15.229.74:8080 groovy --username "admin" --password "11cfd35185f955e0f4f547669ee81ebc7a" = < plugins.groovy > plugins.txt"
+sh "java -jar jenkins-cli.jar -s http://3.15.229.74:8080 groovy --username "admin" --password "11cfd35185f955e0f4f547669ee81ebc7a" = < plugin.groovy > plugins.txt"
 }
