6964841d06234f1d242fdf69a63acf1b67f1b9e7
suneelpunnana <55619931+suneelpunnana@users.noreply.github.com>
2020-02-24 19:22:01 +0530

    Update Build.groovy

--- vars/Build.groovy
+++ vars/Build.groovy
@@ -1,4 +1,5 @@
 def call()
 {
-  sh "curl -s http://52.14.229.175:8080/job/normalpro/lastBuild/api/json -u suneel:11035ac86f58bc32d03d8e873b7cc063a3 -o Build.json"
+    sh "curl -XGET -g http://52.14.229.175:8080/job/normalpro/api/json?tree=builds[number,status,timestamp,id,result] -u suneel:11035ac86f58bc32d03d8e873b7cc063a3"
+ -o Build.json"
 }
