0875bab1e198f9d0f730811ca4d3620a434005f0
suneelpunnana <55619931+suneelpunnana@users.noreply.github.com>
2020-02-25 11:21:02 +0530

    Update Build.groovy

--- vars/Build.groovy
+++ vars/Build.groovy
@@ -1,16 +1,31 @@
 import groovy.json.*
 
 @NonCPS
-create(){
-def jsonSlurper = new JsonSlurper()
-def reader = new BufferedReader(new InputStreamReader(new FileInputStream("/var/lib/jenkins/workspace/normalpro/Build.json"),"UTF-8"))
-def resultJson = jsonSlurper.parse(reader)
-   
-    println("total number of builds:"+resultJson.builds[0].number)
-}
     
     def call()
 {
     sh "curl -XGET -g http://52.14.229.175:8080/job/normalpro/api/json?tree=builds[number,status,timestamp,id,result] -u suneel:11035ac86f58bc32d03d8e873b7cc063a3 -o Build.json"
     create()
+}
+
+create(){
+def jsonSlurper = new JsonSlurper()
+def reader = new BufferedReader(new InputStreamReader(new FileInputStream("/var/lib/jenkins/workspace/normalpro/Build.json"),"UTF-8"))
+def resultJson = jsonSlurper.parse(reader)
+def size = resultJson.builds[0].number
+   
+    println("total number of builds:"+resultJson.builds[0].number)
+   def countS=0;
+   for(int i=0;i<size;i++){
+      if(resultJson.builds[i].result=="SUCCESS"){
+         countS++;
+      }
+      else{
+         countF++;
+         
+      }
+      
+   }
+     
+   
 }
