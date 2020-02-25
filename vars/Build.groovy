10b96e88af0b792657a3b3a5aaffd8ed3e885165
suneelpunnana <55619931+suneelpunnana@users.noreply.github.com>
2020-02-25 11:37:41 +0530

    Update Build.groovy

--- vars/Build.groovy
+++ vars/Build.groovy
@@ -1,13 +1,6 @@
 import groovy.json.*
 
 @NonCPS
-    
-    def call()
-{
-    sh "curl -XGET -g http://52.14.229.175:8080/job/normalpro/api/json?tree=builds[number,status,timestamp,id,result] -u suneel:11035ac86f58bc32d03d8e873b7cc063a3 -o Build.json"
-    create()
-}
-
 create(){
 def jsonSlurper = new JsonSlurper()
 def reader = new BufferedReader(new InputStreamReader(new FileInputStream("/var/lib/jenkins/workspace/normalpro/Build.json"),"UTF-8"))
@@ -31,3 +24,11 @@ def resultJson = jsonSlurper.parse(reader)
      
    
 }
+    
+    def call()
+{
+    sh "curl -XGET -g http://52.14.229.175:8080/job/normalpro/api/json?tree=builds[number,status,timestamp,id,result] -u suneel:11035ac86f58bc32d03d8e873b7cc063a3 -o Build.json"
+    create()
+}
+
+
