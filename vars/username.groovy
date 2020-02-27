8f4ca36deaf53c451154b21806db063f7ba55900
suneelpunnana <55619931+suneelpunnana@users.noreply.github.com>
2020-02-27 23:58:04 +0530

    Update username.groovy

--- vars/username.groovy
+++ vars/username.groovy
@@ -8,7 +8,9 @@ def resultJson = jsonSlurper.parse(reader)
     int countS=0;
    int countF=0;
    def build=resultJson.users[0].project.builds
+ int len=build.length()
   println("total number of builds by suneel:"+build)
+  println("no. of buils:"+len)
    for(int i=0;i<build.length;i++){
       if(resultJson.users[0].project.builds[i].actions[0].causes[0].userName=="suneelpunnana"){
          countS++;
