c6c397d6f5b8191b5242050ebe25dbab2dbedbb7
suneelpunnana <55619931+suneelpunnana@users.noreply.github.com>
2020-02-27 23:44:13 +0530

    Update username.groovy

--- vars/username.groovy
+++ vars/username.groovy
@@ -1,5 +1,27 @@
-    def call()
+ import groovy.json.*
+
+@NonCPS
+create(){
+def jsonSlurper = new JsonSlurper()
+def reader = new BufferedReader(new InputStreamReader(new FileInputStream("/var/lib/jenkins/workspace/normalpro/username.json"),"UTF-8"))
+def resultJson = jsonSlurper.parse(reader)
+    int countS=0;
+   int countF=0;
+    def total=resultJson.users[0].project.builds.length
+   for(int i=0;i<total;i++){
+      if(resultJson.users[0].project.builds[i].actions[0].causes[0].userName=="suneelpunnana"){
+         countS++;
+      }    
+   }
+    println("total number of builds by suneel:"+countS)
+    
+ 
+     
+     
+
+def call()
 {
     sh "curl -XGET -g http://52.14.229.175:8080/asynchPeople/api/json?depth=3 -o username.json"
+    create()
 }
  
