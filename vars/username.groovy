0b516ef3adf5da0e1fe698983de0f112d4b2626b
suneelpunnana <55619931+suneelpunnana@users.noreply.github.com>
2020-02-27 22:44:16 +0530

    Update username.groovy

--- vars/username.groovy
+++ vars/username.groovy
@@ -1,4 +1,5 @@
     def call()
 {
-    sh "curl -XGET -g http://52.14.229.175:8080/asynchPeople/api/json?depth=3"
+    sh "curl -XGET -g http://52.14.229.175:8080/asynchPeople/api/json?depth=3 -o username.json"
 }
+ 
