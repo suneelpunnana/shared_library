f35d5d6c40ac3599e3136bd123625b93c8ef3e3c
suneelpunnana <55619931+suneelpunnana@users.noreply.github.com>
2020-03-02 13:20:02 +0530

    Update Build.groovy

--- vars/Build.groovy
+++ vars/Build.groovy
@@ -3,7 +3,7 @@ import groovy.json.*
 @NonCPS
 create(){
 def jsonSlurper = new JsonSlurper()
-def reader = new BufferedReader(new InputStreamReader(new FileInputStream("/var/lib/jenkins/workspace/normalpro/Build.json"),"UTF-8"))
+def reader = new BufferedReader(new InputStreamReader(new FileInputStream("/var/lib/jenkins/workspace/${JOB_NAME}/Build.json"),"UTF-8"))
 def resultJson = jsonSlurper.parse(reader)
 println("total number of builds:"+resultJson.builds[0].number)
    int countS=0;
@@ -27,13 +27,9 @@ println("total number of builds:"+resultJson.builds[0].number)
     
     def call()
 {
-    sh "curl -XGET -g http://52.14.229.175:8080/job/normalpro/api/json?tree=builds[number,status,timestamp,id,result] -u suneel:11035ac86f58bc32d03d8e873b7cc063a3 -o Build.json"
+   sh "curl -XGET -g http://52.14.229.175:8080/job/${JOB_NAME}/api/json?tree=builds[number,status,timestamp,id,result] -u suneel:11035ac86f58bc32d03d8e873b7cc063a3 -o Build.json"
     create()
 }
 
-def result = ​parsedjson.findAll { it.value instanceof List } 
-          .values()                                          
-          .flatten()                                         
-          .collect { [it.id, it.result] }  
 
 
