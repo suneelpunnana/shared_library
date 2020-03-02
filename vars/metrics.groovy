47d25c783ffe8910d928f08a98bb6869eb0d196c
suneelpunnana <55619931+suneelpunnana@users.noreply.github.com>
2020-03-02 16:03:24 +0530

    Update metrics.groovy

--- vars/metrics.groovy
+++ vars/metrics.groovy
@@ -5,7 +5,7 @@ create(){
 def jsonSlurper = new JsonSlurper()
 def reader = new BufferedReader(new InputStreamReader(new FileInputStream("/var/lib/jenkins/workspace/normalpro/username.json"),"UTF-8"))
 def resultJson = jsonSlurper.parse(reader)
- if(resultJson.users[1].user.property[10].address=="suneelpunnana3@gmail.com"){
+ if(resultJson.users[1].user.property[9].address=="suneelpunnana3@gmail.com"){
   def name=users[1].user.fullName
   println(name)
  }
