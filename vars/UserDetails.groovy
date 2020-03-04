91a4089311cd598e0cb38d04f85e6c1cd6627701
suneelpunnana <suneelpunnana3@gmail.com>
2020-03-04 11:42:43 +0530

    Update UserDetails.groovy

--- vars/UserDetails.groovy
+++ vars/UserDetails.groovy
@@ -13,7 +13,7 @@ def mailcount = jsonObj.config.emails.email.size()
 
 sh "curl -X GET -g http://52.14.229.175:8080/job/jenkins/api/json?tree=builds[id,result,changeSets[items[authorEmail]]] -u suneel:11035ac86f58bc32d03d8e873b7cc063a3 -o username.json"
 	def jsonSlurper = new JsonSlurper()
-def reader = new BufferedReader(new InputStreamReader(new FileInputStream("/var/lib/jenkins/workspace/normalpro/username.json"),"UTF-8"))
+def reader = new BufferedReader(new InputStreamReader(new FileInputStream("/var/lib/jenkins/workspace/UserDetails/username.json"),"UTF-8"))
 def resultJson = jsonSlurper.parse(reader)
 	def build=resultJson.builds[0].id
 	print(build)
