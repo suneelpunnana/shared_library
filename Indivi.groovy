8c6683d4ff3d6de9e6e531f41e7e42e4c9d8ccd3
suneelpunnana <suneelpunnana3@gmail.com>
2020-03-11 18:32:47 +0530

    Create Indivi.groovy

---
+++ Indivi.groovy
@@ -0,0 +1,66 @@
+import groovy.json.*
+
+def call(jsondata,bitbucket,jenkins,sonar){
+def jsonString = jsondata
+def jsonObj = readJSON text: jsonString
+int ecount = jsonObj.riglet_info.auth_users.size()
+	def team=jsonObj.riglet_info.name
+List<String> jsonStringa= new ArrayList<String>();
+  jsonStringa.add(bitbucket)
+   jsonStringa.add(jenkins)
+	jsonStringa.add(sonar)
+   List<String> LIST = new ArrayList<String>();
+   for(i=0;i<jsonStringa.size();i++)
+  { 
+    int score=0
+    String name="  "
+	  String metric=" "
+    if(jsonStringa[i].contains("bitbucket"))
+    {
+      name="bitbucket"
+	  //  metric="commits"
+//def jsonStringa = bitbucket
+def jsonObja = readJSON text: jsonStringa[i]
+int total=jsonObja.bitbucket.Commit_count
+ // println(jsonObja)
+  //println(total)
+ 
+	    LIST.add(["toolName":name,"metric":"commits","value":total])
+      }
+      if(jsonStringa[i].contains("JENKINS"))
+    {
+      name="jenkins"
+      def jsonObjb = readJSON text: jsonStringa[i]
+	   // print jsonObjb
+      def total=jsonObjb.JENKINS.teambuild_cnt
+  def scnt =jsonObjb.JENKINS.teamsuccessbuild_cnt
+	    def fcnt=jsonObjb.JENKINS.teamfailurebuild_cnt
+	    LIST.add(["toolName":name,"metricName":"total_builds","value":total])
+	    LIST.add(["toolName":name,"metricName":"successful_builds","value":scnt])
+	    LIST.add(["toolName":name,"metricName":"failure_builds","value":fcnt])
+      }
+      if(jsonStringa[i].contains("Sonar"))
+    {
+	    name="sonar"
+	    def jsonObjc = readJSON text: jsonStringa[i]
+	    //print jsonObjc
+	    for(i=0;i<jsonObjc.Sonar.Metrics.component.measures.size();i++){
+		    //print jsonObjc.Sonar.Metrics.component.measures
+    def sonar_metric=jsonObjc.Sonar.Metrics.component.measures[i].metric
+		    def d=jsonObjc.Sonar.Metrics.component.measures[i].value
+    double data = Double.parseDouble(d); 
+       LIST.add(["toolName":name,"metricName":sonar_metric,"value":data])
+	    }
+    }
+     }
+def jsonBuilder = new groovy.json.JsonBuilder()
+
+jsonBuilder(
+ "teamName":team,
+  "metrics" : LIST
+  
+) 
+  
+  File file = new File("/var/lib/jenkins/workspace/${JOB_NAME}/Teamscore.json")
+file.write(jsonBuilder.toPrettyString())	
+}
