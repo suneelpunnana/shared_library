f502bb8ad4734b885d9f2ec645f30a515c4b1f63
suneelpunnana <suneelpunnana3@gmail.com>
2020-03-20 15:10:43 +0530

    Update Build.groovy

--- vars/Build.groovy
+++ vars/Build.groovy
@@ -27,7 +27,7 @@ println("total number of builds:"+resultJson.builds[0].number)
     
     def call()
 {
-   sh "curl -XGET -g http://52.14.229.175:8080/job/${JOB_NAME}/api/json?tree=builds[number,status,timestamp,id,result] -u suneel:11035ac86f58bc32d03d8e873b7cc063a3 -o Build.json"
+   sh "curl -XGET -g http://52.14.229.175:8080/job/${JOB_NAME}/api/json?tree=builds[number,status,timestamp,id,result] -u bhavya:1126482a1b9125e89e2cdeac17df05ddb0 -o Build.json"
     create()
 }
 
